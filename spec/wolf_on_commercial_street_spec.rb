e7bb81198345d6521a2107c7d5a79ba37c4e8945
Simon Legg <leggsimon@gmail.com>
2015-06-09 13:43:14 +0100

    Added test for wolf_on_commercial_street.rb

--- spec/wolf_on_commercial_street_spec.rb
+++ spec/wolf_on_commercial_street_spec.rb
@@ -1,1 +1,7 @@
 require 'wolf_on_commercial_street'
+
+describe 'Stockchecker' do
+  it "checks most prifitable days" do
+    expect(wolf_on_commercial_street([10,2,5,8,9,1,6,7,9,8])).is eq "Buy day: 5, Sell day: 9"
+  end
+end
